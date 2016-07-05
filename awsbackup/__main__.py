c6b83712ced9885fa6e7a670e7522a69b9cf4f83
Dan Tagg <dantagg@wildmanherring.com>
2016-07-05 11:29:04 +0100

    create user  plus attempt at cleanup after running script.

--- awsbackup/__main__.py
+++ awsbackup/__main__.py
@@ -1,10 +1,14 @@
 from __future__ import absolute_import, division, print_function
 import os
+from jinja2 import Environment, PackageLoader
+
 import click
 import boto3
 from botocore.client import ClientError
 
 DEFAULT_AWS_PROFILE = 'default'
+
+env = Environment(loader=PackageLoader('awsbackup', 'templates'))
 
 class AwsBackup(object):
     def __init__(self, home=None, profile=''):
@@ -20,34 +24,57 @@ class BucketExistsError(Exception):
 
 @click.group()
 @click.version_option()
-@click.option('--profile', '-p', default=lambda: os.environ.get('AWS_PROFILE', DEFAULT_AWS_PROFILE))
+@click.option('--profile', '-p', default=lambda: os.environ.get('AWS_PROFILE', DEFAULT_AWS_PROFILE),
+    help="tell awsbackup which aws profile to use from your aws credential file, by default it will use '%s'"
+         % (DEFAULT_AWS_PROFILE,))
 @click.pass_context
 def main(ctx, profile):
     ctx.obj = AwsBackup(profile=profile)
 
 
 @main.command()
-@click.option('--bucket', '-b', prompt=True, default=lambda: os.environ.get('AWS_S3_BUCKET', ''))
+@click.option('--bucket', '-b', prompt=True, default=lambda: os.environ.get('AWS_S3_BUCKET', ''),
+              help='tell awsbackup what to call the bucket to send backups to')
+@click.option('--user', '-u', prompt=True,
+              help='tell awsbackup what user to create for the server to use to backup with')
 @click.pass_context
-def create(ctx, bucket):
-    profile = ctx.obj.profile
+def create(ctx, bucket, user):
+
+    policy_template = env.get_template('backup_user_policy.json')
+    backup_policy = policy_template.render(bucket=bucket)
+    backup_policy_name = user+'_access_policy'
+
+    profile = ctx.obj.profile   # get the profile from the parent command's options
     session = boto3.Session(profile_name=profile)
-    client = session.client('s3')
+
+    s3_client = session.client('s3')
     try:
-        bl = client.get_bucket_location(Bucket=bucket)
-        raise BucketExistsError("Bucket %s already exists!" % (bucket,))
+        bl = s3_client.get_bucket_location(Bucket=bucket)
+        raise BucketExistsError("Bucket %s already exists!" % (bucket,))  # this bucket has been created already
     except ClientError as ce:
         if ce.response['Error']['Code'] == 'NoSuchBucket':
-            pass
+            pass    # the bucket doesn't exist, phew
         elif ce.response['Error']['Code'] == 'AllAccessDisabled':
-            raise BucketExistsError("Bucket %s already exists!" % (bucket,))
+            raise BucketExistsError("Bucket %s already exists with a differnet owner!" % (bucket,))  # someone else has a bucket with this name
         else:
             raise ce
 
+    bucket_rc = s3_client.create_bucket(Bucket=bucket)
 
+    iam_client = session.client('iam')
+    usr = iam_client.create_user(UserName=user)
+    usr_policy = iam_client.put_user_policy(UserName=user, PolicyName=backup_policy_name, PolicyDocument=backup_policy)
+    usr_keys = iam_client.create_access_key(UserName=user)
 
-    bucket_rc = client.create_bucket(Bucket=bucket)
     import pdb; pdb.set_trace()
-    print("Hello World! %s" % (bucket_rc['Location'],))
+
+    cleanup(session, bucket, user, backup_policy_name, usr_keys['AccessKey']['AccessKeyId'])
 
 
+def cleanup(session, bucket, user, backup_policy_name, key_id):
+    client = session.client('s3')
+    client.delete_bucket(Bucket=bucket)
+    client = session.client('iam')
+    client.delete_user_policy(UserName=user, PolicyName=backup_policy_name)
+    client.delete_access_key(UserName=user,AccessKeyId=key_id)
+    client.delete_user(UserName=user)
